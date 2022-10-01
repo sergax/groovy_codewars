a4e89a29ee30f2d2a83187e9828e7a43e6d3058b
sergax <sergey.aksenchenko@gmail.com>
2022-10-01 22:31:06 +0300

    added Potatoes

---
+++ src/main/groovy/codewars/Potatoes.groovy
@@ -0,0 +1,19 @@
+package codewars
+
+import org.junit.Test
+
+class Potatoes {
+    static int potatoes(int p0, int w0, int p1) {
+        w0 * Math.round(100 - p0)/Math.round(100 - p1)
+    }
+
+    private static void dotest(int p0, int w0, int p1, int expected) {
+        equals(expected, potatoes(p0, w0, p1));
+    }
+
+    @Test
+    public void test() {
+        dotest(82, 127, 80, 114);
+        dotest(93, 129, 91, 100);
+    }
+}
